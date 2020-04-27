525a179864ec12034f5d0fef1838f5fc1d78f407
okadath <vintaw.01@gmail.com>
2020-04-27 15:56:24 -0500

    {message}

--- autocommit.py
+++ autocommit.py
@@ -1,8 +1,11 @@
 import subprocess as cmd
 from  time import sleep
 while True:
-	sleep(30)
-	cp = cmd.run("git add .", check=True, shell=True)
-	message = "update the repository"
-	cp = cmd.run("git commit -m '{message}'", check=True, shell=True)
-	cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+	try:
+		sleep(30)
+		cp = cmd.run("git add .", check=True, shell=True)
+		message = "update the repository"
+		cp = cmd.run("git commit -m '{message}'", check=True, shell=True)
+		cp = cmd.run("git push -u origin master -f", check=True, shell=True)
+	except Exception as e:
+		pass
